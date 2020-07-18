a9a7348fb1f46656b5ca680c744cbc65e7f40dfb
Joe Stanley <engineerjoe440@yahoo.com>
2020-07-18 15:21:37 -0700

    updated imaging and script

--- common/branding/1 - AutoZipper.py
+++ common/branding/1 - AutoZipper.py
@@ -12,6 +12,16 @@
 
 import os
 import zipfile
+import argparse
+
+# Create Parser Object
+parser = argparse.ArgumentParser(description="`AutoZipper.py` " + 
+    "A simple Python script to package all imaging files in a zipped " +
+    "folder structure in preparation to upload to GitHub for distribution.")
+parser.add_argument('-s','--source',default=os.getcwd(),
+    help="the fully-qualified directory path to locate files from")
+parser.add_argument('-o','--output',default='KrncBranding.zip',
+    help="the zipped folder structure which should be created")
 
 # Zip Files
 def zipdir(path, ziph, extpath=''):
@@ -20,8 +30,16 @@ def zipdir(path, ziph, extpath=''):
             # Collect all Imaging Files
             ziph.write(os.path.join(extpath, file))
 
+# Main Function
+def main( parser ):
+    args = parser.parse_args()
+    src = args.source
+    out = args.output
+    # Create Zipfile Object and Store Files
+    with zipfile.ZipFile(out, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipdir(src, zipf)
+
+
 # Run Main Operation
 if __name__ == '__main__':
-    zipf = zipfile.ZipFile('KrncBranding.zip', 'w', zipfile.ZIP_DEFLATED)
-    zipdir('.', zipf)
-    zipf.close()
+    main( parser )
